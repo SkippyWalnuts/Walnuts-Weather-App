70d9aeb7cd3b8198eac715ca1d6f1c43635fc8e6
SkippyWalnuts <124161796+SkippyWalnuts@users.noreply.github.com>
2023-03-18 10:54:49 +0000

    Added live data to JS for weather conditions, changed dayTime to H5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-let apiKey = "64469ac67e6dc941feb5b50915a18dc7";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayWeather);
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "6dt04340acdo33333a0be9731ef54b37";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
